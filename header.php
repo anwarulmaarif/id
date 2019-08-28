c4e547cc8b1221b9fcb908e8e3ce9534595ccdc7
anwarulmaarif <sip.anwa@gmail.com>
2019-08-28 18:19:46 +0700

    waw

--- header.php
+++ header.php
@@ -2,18 +2,26 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="public/css/custom.css" >
 <link rel="stylesheet" href="public/css/bootstrap.css" >
+<link rel="stylesheet" href="public/css/bootstrap.min.css" >
 <link rel="shortcut icon" href="public/img/lionred.png">
     <title>Lion Parcel Lampung</title>
   </head>
  <body>
 
-<!-- Image and text -->
-<nav class="navbar navbar-light bg-dark" style="opacity:0.2">
-  <a class="navbar-brand text-white" href="#">
-    <img src="public/img/lion.png" width="30" height="30" class="d-inline-block align-top " alt="Home">
-    Lion Parcel Lampung
-  </a>
-</nav>
+<header>
+    <div class="header-banner">
+        <h1>Lion Parcel Lampung</h1>
+    </div>
+    <div class="clear"></div>
+    <nav>
+        <div class="site-title">Lion Parcel Lampung</div>
+        <ul>
+            <li><a href="/archive">Lokasi</a></li>
+            <li><a href="/Promo">Promo</a></li>
+            <li><a href="/Contact">Contact</a></li>
+        <ul>
+    </nav>
+</header>
