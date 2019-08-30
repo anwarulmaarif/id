f1d893cefa203ed9343db015745373f6076b42cd
anwarulmaarif <sip.anwa@gmail.com>
2019-08-30 23:38:58 +0700

    episode 6

--- app/view/about/map.php
+++ app/view/about/map.php
@@ -1,6 +1,6 @@
 <p></p>
 <div class="jumbotron align-content-center w-95 ">
-	<iframe class="container align-content-center w-95" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.0986926806563!2d105.28087601412993!3d-5.4019334554804095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40db09524edc6f%3A0x3544ebc6bfcc5ac0!2sLION%20PARCEL%20LAMPUNG!5e0!3m2!1sid!2sid!4v1567144430007!5m2!1sid!2sid" frameborder="0" width="95%" height="500" style="border:0;" allowfullscreen="true"></iframe>
+	<iframe class="container border border-danger rounded align-content-center w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.0986926806563!2d105.28087601412993!3d-5.4019334554804095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40db09524edc6f%3A0x3544ebc6bfcc5ac0!2sLION%20PARCEL%20LAMPUNG!5e0!3m2!1sid!2sid!4v1567144430007!5m2!1sid!2sid" frameborder="0" width="100%" height="500" style="border:0;" allowfullscreen="true"></iframe>
 	<hr class="my-4">
 	<h1>Kantor Lion Parcel Lampung</h1>
 	<p>Jl. Pangeran Antasari No.1E, Kalibalau Kencana, Kedamaian, Kota Bandar Lampung, Lampung 35131</p>
--- app/view/templates/header.php
+++ app/view/templates/header.php
@@ -2,9 +2,25 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="../../public/css/custom.css" >
-<link rel="stylesheet" href="../../public/css/bootstrap.css" >
-<link rel="shortcut icon" href="../../public/img/lionred.png">
+<link rel="stylesheet" href="<?= BASEURL; ?>/css/custom.css" >
+<link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css" >
+<link rel="shortcut icon" href="<?= BASEURL; ?>/img/lionred.png">
     <title><?= $data['judul']; ?></title>
   </head>
  <body>
+
+ 	<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+ <div class="container">
+  <a class="navbar-brand" href="<?= BASEURL; ?>">Lion Parcel Lampung</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="<?= BASEURL; ?>/about/promo">Promo</a>
+      <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About Us</a>
+    </div>
+  </div>
+  </div>
+</nav>
