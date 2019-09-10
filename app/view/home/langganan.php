eff8c80706facc14800b246cd3a9d8368ed2bd85
anwarulmaarif <sip.anwa@gmail.com>
2019-09-11 00:44:14 +0700

    gcg

--- app/view/home/langganan.php
+++ app/view/home/langganan.php
@@ -15,7 +15,9 @@
 <div class="container shadow rounded mb-4 p-4" id="forminput">
 <form action="home/cek" method="post" target="hasil">
 	<div class="form-goup">
-		<label for="noSTT"><h2>Masukan nomor resi anda</h2></label>
+		<label for="noSTT"><h2>Lacak Paket Anda Disini</h2></label>
+		<p style="line-height: 1px; font-size: 10px;">Contoh no resi : 11-19-1234567, 88-19-1234567, 99-19-1234567</p>
+			<p style="line-height: 1px; font-size: 10px;">jangan lupa masukkan tanda "-" (strip)</p>
 		<input type="" name="noSTT" class="form-control" id="noSTT" placeholder="Input Nomor Resi / STT" style="width: 40%;">
 		<button type="submit" class="btn btn-danger mt-2">Submit</button>
 	</div>
